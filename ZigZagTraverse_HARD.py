f939d9c2e639bf2393a2cf0e028bbd8df1e11bb8
Ricky <rickdhanota@gmail.com>
2020-09-06 14:55:35 -0700

    initial commit for this array algo

---
+++ ZigZagTraverse_HARD.py
@@ -0,0 +1,4 @@
+# Write in a function that takes in a n x m 2 dimensiona array (that can be square shaped when n==m) and returns a one dimensional array of all the elements in zig zag order
+# zig zag order starts at the top left corner of the 2 dimensional array, goes down by 1 element, and proceeds in a zig zag order all the way to the bottom right corner
+
+
