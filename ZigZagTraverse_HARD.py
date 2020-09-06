1d98e874f74dc5cfc80d3fe1fef3946d2d4f6b0a
Ricky <rickdhanota@gmail.com>
2020-09-06 16:00:17 -0700

    creating variables

--- ZigZagTraverse_HARD.py
+++ ZigZagTraverse_HARD.py
@@ -1,4 +1,19 @@
 # Write in a function that takes in a n x m 2 dimensiona array (that can be square shaped when n==m) and returns a one dimensional array of all the elements in zig zag order
 # zig zag order starts at the top left corner of the 2 dimensional array, goes down by 1 element, and proceeds in a zig zag order all the way to the bottom right corner
 
+# Example array looks like this:
+# array = [[1, 3, 4, 10]
+#          [2, 5, 9, 11]
+#          [6, 8, 12,15]
+#          [7, 13,14,16]]
+
+# Output would be = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
+def zigzagTraverse(array):
+    height = len(array)-1
+    width = len(array[0])-1
+    result = []
+    row = 0
+    col = 0
+    goingdown = True
 
